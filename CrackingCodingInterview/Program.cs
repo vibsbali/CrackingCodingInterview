f8ad4d5db07ac9ce5a0890a7a09bce8f34320788
Vaibhav Bali <vibsbali@gmail.com>
2016-07-18 19:30:50 +1000

    Completed CommonLongestSubstringProblem

--- CrackingCodingInterview/Program.cs
+++ CrackingCodingInterview/Program.cs
@@ -1,9 +1,14 @@
-﻿namespace CrackingCodingInterview
+﻿using System;
+
+namespace CrackingCodingInterview
 {
     class Program
     {
         static void Main(string[] args)
         {
+            var stringAlgo = new StringAlgorithms();
+            var result = stringAlgo.LongestCommonSubstring("abcdef", "abdefghijkabcdefg");
+            Console.WriteLine(result);
         }
     }
 }
---
+++ CrackingCodingInterview/StringAlgorithms.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CrackingCodingInterview
+{
+    public class StringAlgorithms
+    {
+        public string LongestCommonSubstring(string a, string b)
+        {
+            if (string.IsNullOrEmpty(a) || string.IsNullOrEmpty(b))
+            {
+                throw new ArgumentException();
+            }
+
+            int[,] matrix = new int[a.Length + 1, b.Length + 1];
+
+            var max = 0;
+            var row = 0;
+            
+            for (int i = 1; i <= a.Length; i++)
+            {
+                for (int j = 1; j <= b.Length; j++)
+                {
+                    if (a[i - 1] == b[j - 1])
+                    {
+                        matrix[i, j] = 1 + matrix[i - 1, j - 1];
+                        if (matrix[i,j] > max)
+                        {
+                            max = matrix[i, j];
+                            row = i;
+                        }
+                    }
+                }
+            }
+
+            //if there is common substring
+            string substring = a.Substring(row - max, max);
+            return substring;
+        }
+    }
+}
